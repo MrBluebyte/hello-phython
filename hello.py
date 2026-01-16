6145883590d39655c24b7f8b68ab13b12578f523
Mohamed Siyam <krsiyamrahman@gmail.com>
2026-01-16 23:13:15 +0530

    Add greet function to welcome users

---
+++ hello.py
@@ -0,0 +1,5 @@
+def greet():
+    print("Welcome to my GitHub repository")
+    print("This repo was created to evaluate my GitHub skills for the Open University non-verbal communication assignment.")
+
+greet()
